a9a6b3423832b10c1ed7055ea75f606f336eacd3
Tanmay Singh <tanmay.23bai10328@vitbhopal.ac.in>
2026-06-05 04:14:25 +0530

    phase 2 - Leaning the implementation of each library

---
+++ phase2/langchain_practice.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+ 
